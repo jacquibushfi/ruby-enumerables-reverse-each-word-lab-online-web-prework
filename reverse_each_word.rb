c81d990a6024f30c2cb19cce12a8cea84ed0807c
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-09-27 22:06:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
-def reverse_each_word(str)
- array = []
- array = str.split(/ /)
- 
-  
-  
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  test_array = []
+  array.collect do|string| 
+    test_array << string.reverse 
+  end
+  test_array.join(" ")
 end
